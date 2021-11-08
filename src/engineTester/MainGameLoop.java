60d7797c80579e5488a166ae68a7e2fa474f9310
robhaggis <robhiggins2001@hotmail.com>
2021-11-08 12:18:28 +0000

    Texturing using Shaders

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,12 +1,14 @@
 package engineTester;
 
+import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
+import models.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
 
@@ -26,12 +28,21 @@ public class MainGameLoop {
         int[] indices = {0,1,3,
                         3,1,2};
 
-        RawModel model = loader.loadToVAO(vertices, indices);
+        float[] textureCoords = {
+                0,0,
+                0,1,
+                1,1,
+                1,0
+        };
+
+        RawModel model = loader.loadToVAO(vertices, textureCoords,indices);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+        TexturedModel texturedModel = new TexturedModel(model, texture);
 
         while (!Display.isCloseRequested()) {
             renderer.prepare();
             shader.start();
-            renderer.render(model);
+            renderer.render(texturedModel);
             shader.stop();
             DisplayManager.updateDisplay();
         }

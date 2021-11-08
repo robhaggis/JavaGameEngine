d8ea4f2f53f2f456e5e9585a64909f336e4418e7
robhaggis <robhiggins2001@hotmail.com>
2021-11-08 13:24:05 +0000

    Entity Class

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,8 +1,10 @@
 package engineTester;
 
+import entities.Entity;
 import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 
+import org.lwjgl.util.vector.Vector3f;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import models.RawModel;
@@ -37,12 +39,14 @@ public class MainGameLoop {
 
         RawModel model = loader.loadToVAO(vertices, textureCoords,indices);
         ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
-        TexturedModel texturedModel = new TexturedModel(model, texture);
+        TexturedModel staticModel = new TexturedModel(model, texture);
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,0),0,0,0,1);
 
         while (!Display.isCloseRequested()) {
+            entity.increasePosition(0,0,-.002f);
             renderer.prepare();
             shader.start();
-            renderer.render(texturedModel);
+            renderer.render(entity,shader);
             shader.stop();
             DisplayManager.updateDisplay();
         }

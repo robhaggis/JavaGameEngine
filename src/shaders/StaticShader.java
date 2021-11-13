23627a2701b9234d9b6647ef504b204bbe03c985
robhaggis <robhiggins2001@hotmail.com>
2021-11-13 10:53:01 +0000

    Reenaming shader source files to .glsl extention for IDE support

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -9,8 +9,8 @@ import entities.Light;
 
 public class StaticShader extends ShaderProgram{
 	
-	private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
-	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+	private static final String VERTEX_FILE = "src/shaders/vertexShader.glsl";
+	private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.glsl";
 	
 	private int location_transformationMatrix;
 	private int location_projectionMatrix;

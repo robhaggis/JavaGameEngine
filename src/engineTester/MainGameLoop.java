47b8f7391ad040bb4cf4c1c831fab7bb91608ee0
robhaggis <robhiggins2001@hotmail.com>
2021-11-14 13:23:38 +0000

    Multitexturing Support for Terrains

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -21,6 +21,8 @@ import textures.ModelTexture;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
+import textures.TerrainTexture;
+import textures.TerrainTexturePack;
 
 public class MainGameLoop {
 
@@ -28,6 +30,18 @@ public class MainGameLoop {
 
 		DisplayManager.createDisplay();
 		Loader loader = new Loader();
+
+		//*********************TERRAIN TEXTURES****************************
+		TerrainTexture backgroundTexture = new TerrainTexture(loader.loadTexture("grassy"));
+		TerrainTexture rTexture = new TerrainTexture(loader.loadTexture("dirt"));
+		TerrainTexture gTexture = new TerrainTexture(loader.loadTexture("pinkFlowers"));
+		TerrainTexture bTexture = new TerrainTexture(loader.loadTexture("path"));
+
+		TerrainTexturePack texturePack = new TerrainTexturePack(backgroundTexture,rTexture,gTexture,bTexture);
+		TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMapNew"));
+
+
+		//*********************MODELS****************************
 
 		//Simple Tree
 		ModelData tree = OBJFileLoader.loadOBJ("tree");
@@ -46,28 +60,22 @@ public class MainGameLoop {
 		T_fernModel.getTexture().setUseFakeLighting(true);
 		T_fernModel.getTexture().setHasTransparency(true);
 
-		//Grass
-		ModelData grass = OBJFileLoader.loadOBJ("grass");
-		RawModel grassModel = loader.loadToVAO(grass.getVertices(), grass.getTextureCoords(), grass.getNormals(), grass.getIndices());
-		TexturedModel T_grassModel= new TexturedModel(OBJLoader.loadObjModel("grass", loader), new ModelTexture(loader.loadTexture("grassTexture")));
-		T_grassModel.getTexture().setUseFakeLighting(true);
-		T_grassModel.getTexture().setHasTransparency(true);
 
-
+		//****************************************************
 		
 		List<Entity> entities = new ArrayList<Entity>();
 		Random random = new Random();
 		for(int i=0;i<500;i++){
 			entities.add(new Entity(T_treeModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,3));
-			entities.add(new Entity(T_tree2Model, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,random.nextFloat()*1.5f-0.9f));
-			entities.add(new Entity(T_fernModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,random.nextFloat()*1f-0.3f));
-			entities.add(new Entity(T_grassModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,random.nextFloat()*2f-0.3f));
+			entities.add(new Entity(T_tree2Model, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,0.5f));
+			entities.add(new Entity(T_fernModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,0.5f));
+			//entities.add(new Entity(T_grassModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,1));
 		}
 		
 		Light light = new Light(new Vector3f(20000,20000,2000),new Vector3f(1,1,1));
 		
-		Terrain terrain = new Terrain(0,-1,loader,new ModelTexture(loader.loadTexture("grass")));
-		Terrain terrain2 = new Terrain(-1,-1,loader,new ModelTexture(loader.loadTexture("grass")));
+		Terrain terrain = new Terrain(0,-1,loader,texturePack,blendMap);
+		Terrain terrain2 = new Terrain(-1,-1,loader,texturePack,blendMap);
 		
 		Camera camera = new Camera();	
 		MasterRenderer renderer = new MasterRenderer();
--- src/shaders/terrainVertexShader.glsl
+++ src/shaders/terrainVertexShader.glsl
@@ -29,7 +29,7 @@ void main(void){
 	vec4 worldPosition = transformationMatrix * vec4(position,1.0);
 	vec4 positionRelativeToCamera = viewMatrix * worldPosition;
 	gl_Position = projectionMatrix * positionRelativeToCamera;
-	pass_textureCoordinates = textureCoordinates * 40.0;
+	pass_textureCoordinates = textureCoordinates;
 	
 	surfaceNormal = (transformationMatrix * vec4(normal,0.0)).xyz;
 	toLightVector = lightPosition - worldPosition.xyz;

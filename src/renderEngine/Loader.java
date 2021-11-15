c8c8c9f4a8ef99de420da79db9e4194e485df5ed
robhaggis <robhiggins2001@hotmail.com>
2021-11-15 13:26:39 +0000

    Enabled MipMapping

--- src/renderEngine/Loader.java
+++ src/renderEngine/Loader.java
@@ -44,6 +44,7 @@ public class Loader {
 			//NOTE Mipmapping
 			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
 			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.4f);	//TODO move to settings. more negative = higher level of details for downsampled textures
 
 		} catch (Exception e) {
 			e.printStackTrace();
